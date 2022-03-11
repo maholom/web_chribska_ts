25eed3dbfac3ef737ee23db7518ea63ab9cff033
MartaKirchgessner <martakirchgessner@gmail.com>
2022-03-11 18:26:07 +0100

    feat: set up react router

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -6,4 +6,11 @@ describe('Homepage', () => {
     cy.tid('main-text').should('contain', 'Test');
     cy.tide('text').should('contain', 'Test');
   });
+  it('routing', () => {
+    cy.get('h1').should('contain', 'Home');
+    cy.tid('link-about').click();
+    cy.get('h1').should('contain', 'About');
+    cy.tid('link-home').click();
+    cy.get('h1').should('contain', 'Home');
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Missing = () => <h1>Missing</h1>;
 
 function App() {
   return (
     <div className="App">
+      <Link data-testid="link-about" to="/about">
+        About
+      </Link>
+      <Link data-testid="link-home" to="/">
+        Home
+      </Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p data-testid="main-text">
