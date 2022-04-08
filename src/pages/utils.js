f81afd76e66c24dc1c3eb3795b267c3014013676
MartaKirchgessner <martakirchgessner@gmail.com>
2022-04-08 18:32:49 +0200

    fix: resizing of photos

--- src/pages/utils.js
+++ src/pages/utils.js
@@ -1,49 +1,49 @@
-import home_headliner from './homefoto/home_headliner.jpg';
-import home_headliner2 from './homefoto/home_headliner2.jpg';
-import home_headliner3 from './homefoto/home_headliner3.jpg';
-import home_headliner5 from './homefoto/home_headliner5.jpg';
-import home_headliner6 from './homefoto/home_headliner6.jpg';
-import home_headliner7 from './homefoto/home_headliner7.jpg';
-import home_headliner8 from './homefoto/home_headliner8.jpg';
-import home_headliner10 from './homefoto/home_headliner10.jpg';
+import headliner_1 from './homefoto/headliner_1.jpg';
+import headliner_2 from './homefoto/headliner_2.jpg';
+import headliner_3 from './homefoto/headliner_3.jpg';
+import headliner_natur_1 from './homefoto/headliner_natur_1.jpg';
+import headliner_natur2 from './homefoto/headliner_natur2.jpg';
+import headliner_innen_1 from './homefoto/headliner_innen_1.jpg';
+import headliner_innen_2 from './homefoto/headliner_innen_2.jpg';
+import headliner_natur3 from './homefoto/headliner_natur3.jpg';
 
 export const itemDataCollageHome = [
   {
-    img: home_headliner,
+    img: headliner_1,
     title: 'Chalupa',
     rows: 2,
     cols: 2,
   },
   {
-    img: home_headliner7,
+    img: headliner_natur2,
+    title: 'Příroda',
+  },
+  {
+    img: headliner_innen_1,
+    title: 'Interier',
+  },
+  {
+    img: headliner_2,
     title: 'Chalupa',
-  },
-  {
-    img: home_headliner8,
-    title: 'Camera',
-  },
-  {
-    img: home_headliner10,
-    title: 'Coffee',
     cols: 2,
   },
   {
-    img: home_headliner5,
-    title: 'Hats',
+    img: headliner_innen_2,
+    title: 'Interier',
     cols: 2,
   },
   {
-    img: home_headliner6,
-    title: 'Honey',
+    img: headliner_natur_1,
+    title: 'Natur',
     rows: 2,
     cols: 2,
   },
   {
-    img: home_headliner2,
+    img: headliner_natur3,
     title: 'Basketball',
   },
   {
-    img: home_headliner3,
+    img: headliner_3,
     title: 'Fern',
   },
 ];
