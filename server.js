b6c63eb20f426bf0b9a55d83ac23eccd1d012623
MartaKirchgessner <martakirchgessner@gmail.com>
2022-04-11 18:50:19 +0200

    fix: server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, 'public');
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(port, () => {
   console.log('Server is up!');
 });
