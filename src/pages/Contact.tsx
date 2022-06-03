f66c89c02543719495f9fd06a557ddd9ec123ca3
MartaKirchgessner <martakirchgessner@gmail.com>
2022-06-03 15:28:17 +0200

    refactoring

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import { Box } from '@mui/material';
-// import { useIntl } from 'react-intl';
+import { Box, Typography, alpha } from '@mui/material';
+import { useIntl } from 'react-intl';
 import MediaCard from '../components/Card';
 import headliner_innen_1 from './homefoto/headliner_innen_1.jpg';
 import headliner_innen_2 from './homefoto/headliner_innen_2.jpg';
 import { ReservationForm } from '../components/ReservationForm';
+import { theme } from '../theme';
 
 const Contact = () => {
-  // const intl = useIntl();
+  const intl = useIntl();
 
   return (
     <>
       <Box maxWidth="xl">
+        <Container>
+          <Grid container sx={{ justifyContent: 'space-around' }}>
+            <Typography
+              textAlign="center"
+              variant={'h2'}
+              sx={{
+                color: () => alpha(theme.palette.primary.main, 1),
+                mb: 4,
+                mt: 3,
+              }}
+            >
+              {intl.formatMessage({
+                id: 'contact-title',
+              })}
+            </Typography>
+          </Grid>
+        </Container>
         <Container sx={{ m: 2, p: 0 }}>
           <Grid container sx={{ justifyContent: 'space-around' }}>
             <MediaCard
